b90a33f5305bba873c698a2c94759808246b94ca
Nicolai <Nicolaigorden@gmail.com>
2023-10-03 15:39:25 -0400

    initialized user routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,6 @@ import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
 
   return (
     <BrowserRouter>
@@ -18,9 +11,6 @@ function App() {
         <Switch>
           <Route path="/testing">
             <h1>Test Route</h1>
-          </Route>
-          <Route path="/">
-            <h1>Page Count: {count}</h1>
           </Route>
         </Switch>
       </div>
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get '/hello', to: 'application#hello_world'
+  post "/login", to: "sessions#create"
+
+  resources :users, only: [:show, :create]
 
   get '*path',
       to: 'fallback#index',
