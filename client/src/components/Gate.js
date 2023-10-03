e334a767afbe7d3d977a4938194d289ff9644c83
Nicolai <Nicolaigorden@gmail.com>
2023-10-03 19:09:10 -0400

    added gate form

--- client/src/components/Gate.js
+++ client/src/components/Gate.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Gate() {
+    const [username, setUsername] = useState()
+    const [password, setPassword] = useState()
+    
+    function onSubmit(e) {
+        e.preventDefault()
+        if (e.nativeEvent.submitter.name === 'Login') {
+            console.log('logging in!')
+        } else if (e.nativeEvent.submitter.name === 'Sign Up') {
+            console.log('signed up!')
+        }
+    }
+
     return (
-        <div>sign in!</div>
+        <form onSubmit={onSubmit}>
+
+            <label htmlFor="username">Username:</label>
+            <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+
+            <label htmlFor="password">Password:</label>
+            <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button type="submit" name="Login">Log In</button>
+            <button type="submit" name="Sign Up">Sign Up</button>
+
+        </form>
     )
 }
 
