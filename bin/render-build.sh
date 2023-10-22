f7ab46d7253890e36675c5aed9b8c19f275bb7ba
Nicolai <Nicolaigorden@gmail.com>
2023-10-22 00:39:51 -0400

    ...

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,5 +9,5 @@ cp -a client/build/. public/
 
 # Build commands for back end
 bundle install
-bundle exec db:drop db:create db:migrate
+bundle exec db:migrate:reset
 bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
